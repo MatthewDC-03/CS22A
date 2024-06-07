d80597daa8f7b13e625c5ffb8cc665588cc76d45
TheOne-afk <156886567+TheOne-afk@users.noreply.github.com>
2024-06-07 09:41:06 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,7 +142,7 @@ export default function Home() {
     <main className="w-full h-full relative" ref={container}  >
       {/* Flow / Arrow Guide */}
 
-      <div id="arrPinned" className="fixed h-[500px] left-1/2 -translate-x-1/2 w-[3px] bg-[#008DDA]" ></div>
+      <div id="arrPinned" className="fixed h-[100px] left-1/2 -translate-x-1/2 w-[3px] bg-[#008DDA]" ></div>
 
         {/* Navbar */}
       <header className={`navbar w-full ${scrolled ? "bg-[#1E1E1E] h-12" : "bg-[black]/20 h-16 backdrop-blur-sm"} sticky top-0 left-0 z-50`}> 
